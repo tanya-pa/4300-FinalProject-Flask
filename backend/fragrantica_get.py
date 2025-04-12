f1073d1e18b8b4f1a694c51a28fc5f9851459985
Tanya Paul <tanya26.paul@gmail.com>
2025-04-12 12:44:28 -0400

    Update requirements

---
+++ backend/fragrantica_get.py
@@ -0,0 +1,12 @@
+# script to scrape from fragrantica website
+# attributes needed: 
+# - accords
+# - notes (top, bottom, medium) -> merge all in preprocessing
+# - occasion: winter, spring, fall, summer, day/night time (presumably integers)
+# - brand (that they already like)
+# - gender  (man, woman, unisex) 
+# - rating
+# - number of votes
+# - listed prices (if available)
+# - longevity (if available)
+
